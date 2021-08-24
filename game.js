51ce77397e410f58c9b43a28447bb41bde016500
tpinto01 <pintoleitetomas@gmail.com>
2021-08-23 23:40:32 -0400

    Succefful socketio connection

--- game.js
+++ game.js
@@ -1,28 +1,34 @@
+const randomstring = require
 const uuidv4 = require('uuid').v4;
 
 class Connection {
   constructor(io, socket) {
     this.socket = socket;
     this.io = io;
+    this.players = {};
 
-    socket.on('choice', (choice) => this.handleChoice(choice));
+    socket.on('create-game', (data) => this.createGame(data));
     socket.on('disconnect', () => this.disconnect());
     socket.on('connect_error', (err) => {
       console.log(`connect_eror due to ${err.message}`);
     });
   }
 
-  sendChoice(choice) {
-    this.io.sockets.emit('choice', choice);
+  createGame(data) {
+    const roomID=randomstring.generate({length: 4});
+    this.io.sockets.join(roomID);
+    players[roomID]=data.name;
+    this.io.sockets.emit('newGame', {roomID:roomID});    
   }
+
 }
 
 
-function room(io) {
+function game(io) {
   io.on('connection', (socket) => {
     new Connection(io, socket);
     console.log("Socket.io connection");
   });
 };
 
-module.exports = room;
+module.exports = game;
---
+++ public/javascripts/client.js
@@ -0,0 +1,7 @@
+const socket = io();
+
+const rockButton = document.getElementById('rock-button');
+const paperButton = document.getElementById('paper-button');
+const scissorsButton= document.getElementById('scissors-button');
+
+
